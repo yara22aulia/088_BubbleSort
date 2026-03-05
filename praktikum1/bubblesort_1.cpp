9ed3a5a01ada576f8986ea380624ef125d3198af
yara22aulia <yara22aulia@gmail.com>
2026-03-05 18:28:51 +0700

    step 1

---
+++ praktikum1/bubblesort_1.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int arr[20]; 
+int n;
+
+void input()
+{ // procedur untuk input
+    int d;
+    while (true)
+    {
+        cout << "Masukan banyaknya elemen pada array : ";
+        cin >> n;
+        if (n <= 20)
+        break;
+        else
+        {
+            cout << "\nArray dapat maksimal 20 elemen.\n";
+        }
+    }
+    cout << endl;
+    cout << "===============" << endl;
+    cout << "Masukan Elemen Array" << endl;
+    cout << "===============" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i]
+    }
+}
+
+void bubbleSortArray()
+{
+    int pass = 1;
+
+    do
+    {
+        for (int j = 0; j <= n - 1 - pass; j++)
+        {
+            if (arr[j] >arr[j + 1])
+            {
+                int temp;
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+        pass += 1; 
+
+        cout << "\nPass " << pass - 1 << ": ";
+        for (int k = 0; k < n; k++)
+        {
+            cout << arr[k] << " ";
+        }
+    }
+    
+}
