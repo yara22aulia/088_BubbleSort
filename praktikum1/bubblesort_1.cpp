03f62c05d3830c8e3fc409acc3b76b6bd925758e
yara22aulia <yara22aulia@gmail.com>
2026-03-05 19:20:05 +0700

    b

--- praktikum1/bubblesort_1.cpp
+++ praktikum1/bubblesort_1.cpp
@@ -56,8 +56,39 @@ void bubbleSortArray()
         cout << endl;
 
     } while (pass <= n - 1);
+void display()
+{
+    cout << endl;
+    cout << "================================" << endl;
+    cout << "Element Array yang telah tersusn" << endl;
+    cout << "================================" << endl;
+    cout << endl;
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j];
+        if (j < n - 1)
+        {
+            cout << "--> ";
+        }
+    }
+    cout << endl;
+    cout << endl;
     
+    cout << "Jumlah pass = " << n - 1 << endl; 
+    cout << endl;
+    cout << endl;
+}
+int main()
+{
 
+    input();
+
+    bubbleSortArray();
+    display();
+
+    system("pause");
+    return 0;
+}
 
 
 
